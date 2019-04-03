adef4cd57dcc9a8690c79bf80144bd937a8b068d
lixiaofan1997 <773515092@qq.com>
2019-04-03 17:21:24 +0800

    代码高亮添加删除功能

--- 33-highlight/main.js
+++ 33-highlight/main.js
@@ -1,10 +1,15 @@
 /*global hljs:true*/
 $(function(){
   var $btnAddCode = $('input[type="button"]');
+
   $btnAddCode.click(function(){
-    var $code=$('<div><pre class="javascript"></pre></div>');
+    var $code=$('<div><pre class="javascript"></pre><button class="del">删除</button></div>');
     $code.find('pre').html($('.code').val());
     $('div.main').append($code);
     hljs.highlightBlock($code[0]);
-  });
-});
+    var $del=$code.find('.del');
+    $del.on('click',function(){
+      $code.remove();
+    });
+  }); 
+});  
